220a0dbe8e99056ce37fc3300c5203ba984f0835
kris5498 <chaitanya5498@gmail.com>
2022-05-30 18:17:04 +0530

    added HelloWorld.java file

---
+++ HelloWorld.java
@@ -0,0 +1,9 @@
+//Java Program to print Hello World!!!
+
+public class HelloWorld {
+	public static void main(String args[]) {
+		
+		#This prints Hello World!!!
+		System.out.print("Hello World!!!");
+	}
+}
--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # helloworld
+
+Hello Everyone,
+
+Welcome to the world of Java!!!
+
+This is a simple java program to print Hello World!!!
